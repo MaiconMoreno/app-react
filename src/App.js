ea12f873c4a9656fe60791842e5e33da82e4d2ab
Maicon Moreno <maiconmoreno@hotmail.com>
2020-05-30 20:41:12 -0300

    Atualização da app.js como arrow function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import './styles.css';
+import Header from './Components/Header/index';
 
-function App() {
-  return (
-    <div className="App">
-      <h1> Hello</h1>
-    </div>
-  );
-}
+
+const App = () => (
+  <div className="App">
+    <Header />
+  </div>
+);
+
+
 
 export default App;
